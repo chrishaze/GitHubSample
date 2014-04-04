1070c15026240812bbdbc618805cc6c662d5406a
chrishaze <chrishaze88@gmail.com>
2014-04-04 04:00:48 -0400

    Initial Commit
    Starting Project

---
+++ Zaved/ZavedTests/ZavedTests.m
@@ -0,0 +1,34 @@
+//
+//  ZavedTests.m
+//  ZavedTests
+//
+//  Created by Chris Hayes on 4/4/14.
+//  Copyright (c) 2014 CAV. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZavedTests : XCTestCase
+
+@end
+
+@implementation ZavedTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
